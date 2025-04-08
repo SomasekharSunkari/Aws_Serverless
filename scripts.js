205420c42f614ca2cc71e8994a9845e0fd9fbb1d
Somasekhar <sunkarisekhar36@gmail.com>
2025-04-08 08:47:42 +0530

    Final Update

--- scripts.js
+++ scripts.js
@@ -1,8 +1,6 @@
-// Add your API endpoint here
-var API_ENDPOINT = "API_ENDPOIND_PASTE_HERE";
+var API_ENDPOINT = "https://abzi8oheol.execute-api.ap-south-1.amazonaws.com/sekhar";
 
-// AJAX POST request to save student data
-document.getElementById("savestudent").onclick = function(){
+document.getElementById("savestudent").onclick = function () {
     var inputData = {
         "studentid": $('#studentid').val(),
         "name": $('#name').val(),
@@ -12,7 +10,7 @@ document.getElementById("savestudent").onclick = function(){
     $.ajax({
         url: API_ENDPOINT,
         type: 'POST',
-        data:  JSON.stringify(inputData),
+        data: JSON.stringify(inputData),
         contentType: 'application/json; charset=utf-8',
         success: function (response) {
             document.getElementById("studentSaved").innerHTML = "Student Data Saved!";
@@ -23,15 +21,14 @@ document.getElementById("savestudent").onclick = function(){
     });
 }
 
-// AJAX GET request to retrieve all students
-document.getElementById("getstudents").onclick = function(){  
+document.getElementById("getstudents").onclick = function () {
     $.ajax({
         url: API_ENDPOINT,
         type: 'GET',
         contentType: 'application/json; charset=utf-8',
         success: function (response) {
             $('#studentTable tr').slice(1).remove();
-            jQuery.each(response, function(i, data) {          
+            jQuery.each(response, function (i, data) {
                 $("#studentTable").append("<tr> \
                     <td>" + data['studentid'] + "</td> \
                     <td>" + data['name'] + "</td> \
